a05d74fcd8297098e37c9a6d53849686dad05055
Martin-Juncos <prof.mcjuncos@gmail.com>
2024-11-25 19:01:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Error404 from "./components/Error/Error404";
+import Footer from "./components/FooterSection/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="layout">
+      <header className="navbar">
+        <Navbar />
+      </header>
+      <div className="main-content">
+        <aside className="sidebar">
+          <Sidebar />
+        </aside>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </main>
+      </div>
+      <footer className="footer">
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
